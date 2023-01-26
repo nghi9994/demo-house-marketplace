c63e4d5ed6bcfbf4ef80d0642ba9321508219cb5
Nghi Nguyen <nghi.nguyen@evehr.com>
2023-01-26 16:17:30 +0700

    Init project

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>House Marketplace</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAze5LRiGlBscyl4MH4BLuc6lesphYopYU",
+  authDomain: "house-marketplace-app-f154d.firebaseapp.com",
+  projectId: "house-marketplace-app-f154d",
+  storageBucket: "house-marketplace-app-f154d.appspot.com",
+  messagingSenderId: "774735919920",
+  appId: "1:774735919920:web:631018ef0b010c917326f5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
