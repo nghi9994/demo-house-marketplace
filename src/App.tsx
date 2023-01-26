ff3cab92a1cc735275d571372635dd0f99c506b3
Nghi Nguyen <nghi.nguyen@evehr.com>
2023-01-26 19:41:41 +0700

    Feat: Create route & navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Navbar } from './components';
+import { Explore, Offers, SignIn, SignUp } from './pages';
 
 function App() {
   return (
     <>
-      <div>App</div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+        <Navbar />
+      </Router>
     </>
   );
 }
