7865c7e14e988748f964c7bb775ddff3b57ff776
Nghi Nguyen <nghi.nguyen@evehr.com>
2023-01-26 17:37:47 +0700

    Init project with firebase

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      <h1>House Marketplace</h1>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/pages/index.ts
@@ -0,0 +1,13 @@
+import {Explore} from './Explore/Explore'
+import {ForgotPassword} from './ForgotPassword/ForgotPassword'
+import {Offers} from './Offers/Offers'
+import {SignIn} from './SignIn/SignIn'
+import { SignUp } from './SignUp/SignUp'
+
+export {
+  Explore,
+  ForgotPassword,
+  Offers,
+  SignIn,
+  SignUp,
+}
